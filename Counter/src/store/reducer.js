09e37cc9cb6db37c06fc61449165cddc46d7750d
mbudnikm <mbudnikm97@gmail.com>
2020-02-04 12:11:49 +0100

    started working on counter

---
+++ Counter/src/store/reducer.js
@@ -0,0 +1,27 @@
+const initialState = {
+    counter: 0
+}
+
+const reducer = (state = initialState, action) => {
+    if(action.type === "INCREMENT") {
+        return {
+            counter: state.counter + 1
+        }
+    } else if (action.type === "DECREMENT") {
+        return {
+            counter: state.counter - 1
+        }
+    } else if (action.type === "ADD5") {
+        return {
+            counter: state.counter + 5
+        }
+    } else if (action.type === "SUBSTRACT5") {
+        return {
+            counter: state.counter - 5
+        }
+    }
+
+    return state
+}
+
+export default reducer
